0951b50fd8ab042510ed155984d33c9cc5803a0f
Eleanore Clark <eleanoreclarkpdx@gmail.com>
2023-02-28 20:20:31 -0700

    add about page

---
+++ scripts/About.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export const AboutPage = (props: any) => {
+    return (
+        <div>
+            <h1>About Page</h1>
+        </div>
+    )
+}
+
+export default AboutPage;
