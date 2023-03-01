3d8e078098a05c8f95959020467aedc9f68464d5
Eleanore Clark <eleanoreclarkpdx@gmail.com>
2023-02-28 21:00:20 -0700

    add backgrounds, routing to about & contact

--- scripts/About.tsx
+++ scripts/About.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
+import {Link} from "react-router-dom";
 
 export const AboutPage = (props: any) => {
     return (
-        <div>
-            <h1>About Page</h1>
+        <div id="about-background">
+            <div className="simple-info-box">
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium ipsa quia deleniti consectetur fugit recusandae accusantium numquam! Similique eligendi atque ipsum corporis quibusdam, a eum, minima dolore quae placeat illo?</p>
+                <Link to="/" className="router-link">
+                    <button>Return to the shop</button> 
+                </Link>
+            </div>
         </div>
     )
 }
--- scripts/Contact.tsx
+++ scripts/Contact.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
+import {Link} from "react-router-dom";
 
 export const ContactPage = (props: any) => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div id="contact-background">
+            <div className="simple-info-box">
+                <h1>Contact</h1>
+                <p>Phone: XXX-XXX-XXXX</p>
+                <p>Email: xx@xxx.com</p>
+                <p>[Insert social media site here]</p>
+                <Link to="/" className="router-link">
+                    <button>Return to the shop</button> 
+                </Link>
+            </div>
         </div>
     )
 }
