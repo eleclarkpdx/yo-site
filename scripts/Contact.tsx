c1e86a57c07db095d3fe285dc617ee4fd05513d1
Eleanore Clark <eleanoreclarkpdx@gmail.com>
2023-02-28 20:20:24 -0700

    add contact page

---
+++ scripts/Contact.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export const ContactPage = (props: any) => {
+    return (
+        <div>
+            <h1>Contact</h1>
+        </div>
+    )
+}
+
+export default ContactPage;
